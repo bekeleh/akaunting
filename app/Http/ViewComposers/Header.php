2cfb6ffe303f095ebf9e524a25edca8c3e4f9f48
Bekele H <bekele.haile@gmail.com>
2020-11-25 11:43:51 +0300

    view composer header user objects

--- app/Http/ViewComposers/Header.php
+++ app/Http/ViewComposers/Header.php
@@ -14,7 +14,7 @@ class Header
     /**
      * Bind data to the view.
      *
-     * @param  View  $view
+     * @param View $view
      * @return void
      */
     public function compose(View $view)
@@ -27,39 +27,40 @@ class Header
         $items_reminder = [];
         $notifications = 0;
         $company = null;
+        if ($user) {
+            // Get customer company
+            if ($user->customer()) {
+                $company = (object)[
+                    'company_name' => setting('general.company_name'),
+                    'company_email' => setting('general.company_email'),
+                    'company_address' => setting('general.company_address'),
+                    'company_logo' => setting('general.company_logo'),
+                ];
+            }
 
-        // Get customer company
-        if ($user->customer()) {
-            $company = (object)[
-                'company_name' => setting('general.company_name'),
-                'company_email' => setting('general.company_email'),
-                'company_address' => setting('general.company_address'),
-                'company_logo' => setting('general.company_logo'),
-            ];
-        }
+            $undereads = $user->unreadNotifications;
 
-        $undereads = $user->unreadNotifications;
+            foreach ($undereads as $underead) {
+                $data = $underead->getAttribute('data');
 
-        foreach ($undereads as $underead) {
-            $data = $underead->getAttribute('data');
-
-            switch ($underead->getAttribute('type')) {
-                case 'App\Notifications\Expense\Bill':
-                    $bills[$data['bill_id']] = $data['amount'];
-                    $notifications++;
-                    break;
-                case 'App\Notifications\Income\Invoice':
-                    $invoices[$data['invoice_id']] = $data['amount'];
-                    $notifications++;
-                    break;
-                case 'App\Notifications\Common\Item':
-                    $items[$data['item_id']] = $data['name'];
-                    $notifications++;
-                    break;
-                case 'App\Notifications\Common\ItemReminder':
-                    $items_reminder[$data['item_id']] = $data['name'];
-                    $notifications++;
-                    break;
+                switch ($underead->getAttribute('type')) {
+                    case 'App\Notifications\Expense\Bill':
+                        $bills[$data['bill_id']] = $data['amount'];
+                        $notifications++;
+                        break;
+                    case 'App\Notifications\Income\Invoice':
+                        $invoices[$data['invoice_id']] = $data['amount'];
+                        $notifications++;
+                        break;
+                    case 'App\Notifications\Common\Item':
+                        $items[$data['item_id']] = $data['name'];
+                        $notifications++;
+                        break;
+                    case 'App\Notifications\Common\ItemReminder':
+                        $items_reminder[$data['item_id']] = $data['name'];
+                        $notifications++;
+                        break;
+                }
             }
         }
 
